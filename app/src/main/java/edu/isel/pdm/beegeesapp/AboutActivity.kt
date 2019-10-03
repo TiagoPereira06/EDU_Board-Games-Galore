4c572381e67c64478fd22a3fb7f30224f542f6fc
Tiago Pereira <38113135+TiagoPereira06@users.noreply.github.com>
2019-10-03 22:29:23 +0100

    Adicionei Actividade - "SOBRE"

---
+++ app/src/main/java/edu/isel/pdm/beegeesapp/AboutActivity.kt
@@ -0,0 +1,33 @@
+package edu.isel.pdm.beegeesapp
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_about.*
+
+/**
+ * Screen that displays the author information
+ */
+class AboutActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+        fun navigateToGitHub(author : Int) {
+            if(author==1) {
+                val url = Uri.parse(getString(R.string.about_author1gitpage))
+                startActivity(Intent(Intent.ACTION_VIEW, url))
+            }
+            else{
+                val url = Uri.parse(getString(R.string.about_author2gitpage))
+                startActivity(Intent(Intent.ACTION_VIEW, url))
+            }
+        }
+
+        githubLogoa1.setOnClickListener { navigateToGitHub(1) }
+        githubLogoa2.setOnClickListener { navigateToGitHub(2) }
+    }
+}
